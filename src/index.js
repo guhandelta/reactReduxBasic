78c2a59b520665ce165dbbc447cbd5c1a024443b
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-26 18:17:32 -0500

    Basic structure of the working logic of Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+
+
+//Action => Describes what is to be done --> a simple fn() that returns an obj
+const increment = () => {
+    return {
+        type: 'INCREMNT'
+    }
+}
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+//Reducer => Describes how the Actions transfrom the state
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMNT':
+            return state + 1;
+
+        case 'DECREMENT':
+            return state + 1;
+    }
+};
+
+//Store => Holds all the data or the state for the application, which can be accessed from anywhere
+let store = createStore(counter);
+
+// Do something when the state changes => just console logging something in this case
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatching an action
+store.dispatch(increment());
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
